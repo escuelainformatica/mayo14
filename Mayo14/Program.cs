d3d87960739f0c7a0599067da77005b799be9ece
Jorge Castro <jorge.castro.c@gmail.com>
2021-05-14 11:00:22 -0400

    primera version

---
+++ Mayo14/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mayo14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
